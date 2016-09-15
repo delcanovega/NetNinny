484b76ac2984c3284a644fcd6cafbc3962c3751c
delcanovega <delcanovega@gmail.com>
2016-09-15 17:58:45 +0200

    Start over. TODO list on main

---
+++ Socket.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jdelcano on 2016-09-15.
+//
+
+#include "Socket.h"
---
+++ Socket.h
@@ -0,0 +1,14 @@
+//
+// Created by jdelcano on 2016-09-15.
+//
+
+#ifndef NETNINNY_SOCKET_H
+#define NETNINNY_SOCKET_H
+
+
+class Socket {
+
+};
+
+
+#endif //NETNINNY_SOCKET_H
